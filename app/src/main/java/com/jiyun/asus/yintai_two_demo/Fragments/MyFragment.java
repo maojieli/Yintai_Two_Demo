23370b89885f37ad37e3461f3e58a72025a35cd2
hgbiug <969091731@qq.com>
2017-10-17 14:44:09 +0800

    13点42分

--- app/src/main/java/com/jiyun/asus/yintai_two_demo/Fragments/MyFragment.java
+++ app/src/main/java/com/jiyun/asus/yintai_two_demo/Fragments/MyFragment.java
@@ -6,8 +6,9 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
-/**
+/**eeee
  * 当你的才华满足不了你的野心的时候，那么你应该静下心来学习.
  */
 
@@ -15,6 +16,8 @@ public class MyFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Toast.makeText(getActivity(), "ww", Toast.LENGTH_SHORT).show();
         return super.onCreateView(inflater, container, savedInstanceState);
+
     }
 }
