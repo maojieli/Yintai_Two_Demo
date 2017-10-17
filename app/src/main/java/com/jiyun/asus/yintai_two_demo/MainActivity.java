d868404bb7d476af38aef0994015400376e874d2
韩学文 <799767515@qq.com>
2017-10-17 11:47:39 +0800

    韩学文 启动页

--- app/src/main/java/com/jiyun/asus/yintai_two_demo/MainActivity.java
+++ app/src/main/java/com/jiyun/asus/yintai_two_demo/MainActivity.java
@@ -1,14 +1,30 @@
 package com.jiyun.asus.yintai_two_demo;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Handler handler=new Handler();
+    private static final String MYSP = "yitai";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences preferences = getSharedPreferences(MYSP, MODE_PRIVATE);
+                int count = preferences.getInt("Count", 0);
+                if (count==0){
+                    startActivity(new Intent(MainActivity.this,BannerActivity.class));
+                }else {
+                    startActivity(new Intent(MainActivity.this,OverallActivity.class));
+                }
+            }
+        },2000);
     }
 }
