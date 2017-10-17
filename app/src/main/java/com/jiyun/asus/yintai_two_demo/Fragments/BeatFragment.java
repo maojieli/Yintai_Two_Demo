18b02aaddfb2b458d45c002bce1437d5a2ba4bd2
韩学文 <799767515@qq.com>
2017-10-17 10:05:54 +0800

    韩学文

---
+++ app/src/main/java/com/jiyun/asus/yintai_two_demo/Fragments/BeatFragment.java
@@ -0,0 +1,20 @@
+package com.jiyun.asus.yintai_two_demo.Fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * 当你的才华满足不了你的野心的时候，那么你应该静下心来学习.
+ */
+
+public class BeatFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
