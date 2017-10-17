46b4283885b22c523b730b2b373c79d828bcbba9
asus <1872673159@qq.com>
2017-10-17 11:54:20 +0800

    毛洁丽

--- app/src/main/java/com/jiyun/asus/yintai_two_demo/http/RetrofitSettings.java
+++ app/src/main/java/com/jiyun/asus/yintai_two_demo/http/RetrofitSettings.java
@@ -2,7 +2,14 @@ package com.jiyun.asus.yintai_two_demo.http;
 
 import com.google.gson.Gson;
 import com.jiyun.asus.yintai_two_demo.concat.Concat;
+import com.jiyun.asus.yintai_two_demo.http.huancun.BaseInterceptor;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -15,7 +22,19 @@ public class RetrofitSettings {
     private static RetrofitSettings settings;
     private Retrofit retrofit;
     private RetrofitSettings(){
-        retrofit = new Retrofit.Builder().baseUrl(Concat.BASE_URL).addConverterFactory(GsonConverterFactory.create(new Gson()))
+
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+        httpClientBuilder.connectTimeout(3000, TimeUnit.SECONDS);
+
+        httpClientBuilder.addInterceptor(new Interceptor() {
+            @Override
+            public okhttp3.Response intercept(Chain chain) throws IOException {
+                Request request = chain.request();
+                okhttp3.Response originalResponse = chain.proceed(request);
+                return originalResponse.newBuilder().header("key1", "value1").addHeader("key2", "value2").build();
+            }
+        });
+        retrofit = new Retrofit.Builder().client(httpClientBuilder.build()).baseUrl("http://www.baidu.com").addConverterFactory(GsonConverterFactory.create(new Gson()))
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create()).build();
 
     }
