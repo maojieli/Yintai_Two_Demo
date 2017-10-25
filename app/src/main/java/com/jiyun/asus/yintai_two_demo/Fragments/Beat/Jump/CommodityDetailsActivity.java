1461a48b0ff46c197ff847990cad23d8e7527f61
韩学文 <799767515@qq.com>
2017-10-25 20:03:59 +0800

    韩学文 双页面关联

--- app/src/main/java/com/jiyun/asus/yintai_two_demo/Fragments/Beat/Jump/CommodityDetailsActivity.java
+++ app/src/main/java/com/jiyun/asus/yintai_two_demo/Fragments/Beat/Jump/CommodityDetailsActivity.java
@@ -20,6 +20,7 @@ import android.widget.Toast;
 import com.jiyun.asus.yintai_two_demo.Fragments.Beat.Jump.Adapters.CommodityGvAdapter;
 import com.jiyun.asus.yintai_two_demo.Fragments.Beat.Jump.Adapters.MyExpandableListViewAdapter;
 import com.jiyun.asus.yintai_two_demo.Fragments.Beat.Jump.Beans.CommodityBeam;
+import com.jiyun.asus.yintai_two_demo.Fragments.homepage.bigonclick.BigOnClickActivity;
 import com.jiyun.asus.yintai_two_demo.Http.Presenter.MyPresenter;
 import com.jiyun.asus.yintai_two_demo.Http.View.MyView;
 import com.jiyun.asus.yintai_two_demo.Http.tools.BaseParams;
@@ -38,7 +39,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-
+//  holer.tv_list_price_after.getPaint().setFlags(Paint. STRIKE_THRU_TEXT_FLAG); //中划线
 //商品详情页面+1
 public class CommodityDetailsActivity extends AppCompatActivity implements MyView<CommodityBeam>, View.OnClickListener {
 
@@ -125,6 +126,17 @@ public class CommodityDetailsActivity extends AppCompatActivity implements MyVie
                     adapter.setOpen(false);
                     adapter.notifyDataSetChanged();
                 }
+            }
+        });
+        promotion_body.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                 Intent intent=new Intent(CommodityDetailsActivity.this, BigOnClickActivity.class);
+                   intent.putExtra("tag",2);
+                // "data" -> "{"query_string":"P=27006","displaycount":"30","order_type":"5","page_index":"2","keyword":""}"
+
+                startActivity(intent);
+                return false;
             }
         });
 
